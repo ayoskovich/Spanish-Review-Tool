a9ff2b11ba3c436ee37102cd1593264f8b39d911
Anthony <anthony.yoskovich@gmail.com>
2026-03-17 22:54:12 -0500

    Adds basic word listing with react bootstrap

--- frontend/src/components/ListWords.jsx
+++ frontend/src/components/ListWords.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 export default function WordList() {
   const [words, setWords] = useState([])
@@ -7,14 +10,31 @@ export default function WordList() {
     fetch('/api/words/')
     .then(res => res.json())
     .then(data => setWords(data))
-    .catch(err => console.error(err))
   }, [])
 
+  const borderColors ={
+    verb: 'primary',
+    noun: 'success',
+    reflexive_verb: 'danger'
+  }
+
   return (
-    <ul>
-      {words.map(word => (
-        <li key={word.id}>{word.spelling} ({word.definition})</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {words.map(word => (
+          <Card 
+            style={{ width: '18rem' }} 
+            border= { borderColors[word.word_type]}
+            className='my-4'
+          >
+            <Card.Body>
+              <Card.Title>{word.spelling}</Card.Title>
+              <Card.Subtitle classname="mb-2 text-muted">{word.word_type}</Card.Subtitle>
+              <Card.Text>{word.definition}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </ul>
+    </div>
   )
 }
